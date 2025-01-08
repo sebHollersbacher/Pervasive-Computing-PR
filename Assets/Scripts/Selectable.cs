ee1a7412bd1b5ce9363425a1c22879be1db6c3ef
Sebastian Hollersbacher <s.hollersbacher@outlook.at>
2025-01-08 18:35:35 +0100

    Improved scaling a bit

--- Assets/Scripts/Selectable.cs
+++ Assets/Scripts/Selectable.cs
@@ -64,6 +64,18 @@ public class Selectable : MonoBehaviour
     
     public void Scale(Vector3 difference)
     {
-        gameObject.transform.localScale += difference;
+        Vector3 localMovement = gameObject.transform.InverseTransformDirection(difference);
+        Vector3 scaleChange = new Vector3(
+            Mathf.Sign(localMovement.x) * Mathf.Abs(localMovement.x),
+            Mathf.Sign(localMovement.y) * Mathf.Abs(localMovement.y),
+            Mathf.Sign(localMovement.z) * Mathf.Abs(localMovement.z)
+        );
+        
+        Vector3 newScale = gameObject.transform.localScale + scaleChange;
+        newScale.x = Mathf.Max(newScale.x, 0);
+        newScale.y = Mathf.Max(newScale.y, 0);
+        newScale.z = Mathf.Max(newScale.z, 0);
+
+        gameObject.transform.localScale = newScale;
     }
 }
